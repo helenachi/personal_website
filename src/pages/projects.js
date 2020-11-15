d8a35e94e933678257026e8fcff2f696e7e27e72
Helena Chi <hchi124@gmail.com>
2020-11-14 23:23:06 -0500

    gatsby is overkill for static website

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,55 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
 
-const PROJECTS = ["happy hour", "sentiment classifier", "cut to the case"]
-const EXPERIENCES = [
-  {
-    name: "Happy Hour",
-    project_link: "https://github.com/helenachi/doable",
-    org_name: "urspace labs",
-    org_link: "https://labs.urspace.io/",
-    blurb,
-    location: "Remote",
-    period: "Summer 2020",
-  },
-  {
-    name: "a sentiment classifer",
-    project_link: "https://github.com/helenachi/sentiment_analyzer",
-    // org_name: "",
-    // org_link: "",
-    blurb,
-    location: "Champaign, IL",
-    period: "May 2019",
-  },
-  {
-    name: "Philadelphia Reads Web App",
-    project_link: "https://uiuc.hack4impact.org/projects/pr",
-    org_name: "Hack4Impact UIUC",
-    org_link: "https://uiuc.hack4impact.org/",
-    blurb,
-    location: "Champaign, IL",
-    period: "Fall 2018",
-  },
-  {
-    name: "Cut 2 the Case",
-    project_link: "https://uiuc.hack4impact.org/projects/c2tc-1",
-    org_name: "Hack4Impact UIUC",
-    org_link: "https://uiuc.hack4impact.org/",
-    blurb,
-    location: "Champaign, IL",
-    period: "Spring 2018",
-  },
-  {
-    name: "Neighborhood News Bureau Interactive Map",
-    project_link: "https://uiuc.hack4impact.org/projects/nnb-1",
-    org_name: "Hack4Impact UIUC",
-    org_link: "https://uiuc.hack4impact.org/",
-    blurb,
-    location: "Champaign, IL",
-    period: "Fall 2017",
-  },
-]
-
+// const PROJECTS = ["happy", "sentiment", "philareads", "c2tc"]
 const blurb_happy = (
   <p>
     where I developed a mobile app to encourage young professionals and students
@@ -74,14 +26,114 @@ const blurb_philareads = (
   </p>
 )
 
+const blurb_c2tc = (
+  <p>
+    something something worked on creating dynamic table component
+  </p>
+)
+
+const blurb_nnb = (
+  <p>
+    something something worked on creating an input form with multiple media 
+    options for an interactive map web appliction
+  </p>
+)
+const PROJECTS = [
+  {
+    id: "happy",
+    name: "Happy Hour",
+    project_link: "https://github.com/helenachi/doable",
+    org_name: "urspace labs",
+    org_link: "https://labs.urspace.io/",
+    blurb: blurb_happy,
+    location: "Remote",
+    period: "Summer 2020",
+  },
+  {
+    id: "sentiment",
+    name: "a sentiment classifer",
+    project_link: "https://github.com/helenachi/sentiment_analyzer",
+    // org_name: "",
+    // org_link: "",
+    blurb: blurb_sentiment,
+    location: "Champaign, IL",
+    period: "May 2019",
+  },
+  {
+    id: "philareads",
+    name: "Philadelphia Reads Web App",
+    project_link: "https://uiuc.hack4impact.org/projects/pr",
+    org_name: "Hack4Impact UIUC",
+    org_link: "https://uiuc.hack4impact.org/",
+    blurb: blurb_philareads,
+    location: "Champaign, IL",
+    period: "Fall 2018",
+  },
+  {
+    id: "c2tc",
+    name: "Cut 2 the Case",
+    project_link: "https://uiuc.hack4impact.org/projects/c2tc-1",
+    org_name: "Hack4Impact UIUC",
+    org_link: "https://uiuc.hack4impact.org/",
+    blurb: blurb_c2tc,
+    location: "Champaign, IL",
+    period: "Spring 2018",
+  },
+  {
+    id: "nnb",
+    name: "Neighborhood News Bureau Interactive Map",
+    project_link: "https://uiuc.hack4impact.org/projects/nnb-1",
+    org_name: "Hack4Impact UIUC",
+    org_link: "https://uiuc.hack4impact.org/",
+    blurb: blurb_nnb,
+    location: "Champaign, IL",
+    period: "Fall 2017",
+  },
+]
+
+const show_affiliation = (proj) => {
+  return (proj.org_name ? 
+    (<p>
+      with{" "}
+      <a href={proj.org_link} target="_blank">
+        {proj.org_name}
+      </a>
+    </p>)
+    :
+    (<p></p>)
+  )
+}
+
 function Projects() {
   return (
     <Layout>
       <div>
-        <h1>I've worked on...</h1>
+        <h1>I've worked on ...</h1>
+          <ul>
+            {PROJECTS.map(proj => (
+              <div>
+                <p>{proj.name}</p>
+                {show_affiliation(proj)}
+                
+                {proj.blurb}
+                <div>{proj.location + " " + proj.period}</div>
+              </div>
+            ))}
+          </ul>
+          
+          {/* id: "happy",
+    name: "Happy Hour",
+    project_link: "https://github.com/helenachi/doable",
+    org_name: "urspace labs",
+    org_link: "https://labs.urspace.io/",
+    blurb: blurb_happy,
+    location: "Remote",
+    period: "Summer 2020", */}
+
+        {/* <h1>I've worked on...</h1>
         {PROJECTS.map(proj => (
-          <p>{proj}</p>
-        ))}
+          <p>{proj.name}</p>
+        ))} */}
       </div>
     </Layout>
   )
